2cbaf00df7aca48cb9d1c6a70e50a039f84c5760
wangjk <wangjikai77@gmail.com>
2026-08-03 22:15:59 +0800

    fix: 修正 MainFlutterWindow.swift 中的重写虚函数为 layoutIfNeeded

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -8,24 +8,23 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
-    // Completely hide titlebar
+    // Hide native titlebar
     self.titleVisibility = .hidden
     self.titlebarAppearsTransparent = true
     self.isMovableByWindowBackground = true
     self.styleMask.insert(.fullSizeContentView)
 
-    // Remove traffic light buttons from view tree permanently
-    self.standardWindowButton(.closeButton)?.removeFromSuperview()
-    self.standardWindowButton(.miniaturizeButton)?.removeFromSuperview()
-    self.standardWindowButton(.zoomButton)?.removeFromSuperview()
+    self.standardWindowButton(.closeButton)?.isHidden = true
+    self.standardWindowButton(.miniaturizeButton)?.isHidden = true
+    self.standardWindowButton(.zoomButton)?.isHidden = true
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
   }
 
-  override func layout() {
-    super.layout()
+  override func layoutIfNeeded() {
+    super.layoutIfNeeded()
     self.standardWindowButton(.closeButton)?.isHidden = true
     self.standardWindowButton(.miniaturizeButton)?.isHidden = true
     self.standardWindowButton(.zoomButton)?.isHidden = true
