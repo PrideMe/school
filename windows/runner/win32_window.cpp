a087eeac810c50ff65097f5370274366233f9a42
wangjk <wangjikai77@gmail.com>
2026-08-05 21:21:28 +0800

    style: 强制开启 DWM 沉浸式暗黑标题栏与 #111827 契合色，消除 Windows 白条

--- windows/runner/win32_window.cpp
+++ windows/runner/win32_window.cpp
@@ -273,16 +273,11 @@ void Win32Window::OnDestroy() {
 }
 
 void Win32Window::UpdateTheme(HWND const window) {
-  DWORD light_mode;
-  DWORD light_mode_size = sizeof(light_mode);
-  LSTATUS result = RegGetValue(HKEY_CURRENT_USER, kGetPreferredBrightnessRegKey,
-                               kGetPreferredBrightnessRegValue,
-                               RRF_RT_REG_DWORD, nullptr, &light_mode,
-                               &light_mode_size);
+  BOOL enable_dark_mode = TRUE;
+  DwmSetWindowAttribute(window, DWMWA_USE_IMMERSIVE_DARK_MODE,
+                        &enable_dark_mode, sizeof(enable_dark_mode));
 
-  if (result == ERROR_SUCCESS) {
-    BOOL enable_dark_mode = light_mode == 0;
-    DwmSetWindowAttribute(window, DWMWA_USE_IMMERSIVE_DARK_MODE,
-                          &enable_dark_mode, sizeof(enable_dark_mode));
-  }
+  COLORREF caption_color = RGB(17, 24, 39);
+  DwmSetWindowAttribute(window, 35 /* DWMWA_CAPTION_COLOR */,
+                        &caption_color, sizeof(caption_color));
 }
